804ddbf239a47fd54b6a21af88d45ea464cf7e99
ycode-sh <yemibioscientist@gmail.com>
2023-09-12 07:27:00 +0100

    modifications

--- custom_annotations.sh
+++ custom_annotations.sh
@@ -5,14 +5,6 @@
 # $1= gzipped and tabix-indexed gene_annotation_file, $2 = gene_annotation_header, $3 = lineage_annotation_file, $4 = lineage_annotation_header, 
 # $5 = amr_region_file, $6 = amr_region_header, $7 = variable_region_annotation_file, $8 = variable_region_annotation_header $9 = vcf_file
 
-bgzip $1
-
-bgzip $3
-
-bgzip $5
-
-bgzip $7
-
 
 file_name=$(basename $9 .ann.vcf)
 bcftools annotate --annotations  ${3} --header-lines $4  --columns CHROM,from,to,lineage $9 > ${file_name}_la.vcf
